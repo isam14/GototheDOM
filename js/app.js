5694e0d05dd9782341f6561516264b4ad1e2c5cc
isam14 <imuck14@gmail.com>
2017-09-16 18:22:48 +0200

    resté en suspend,finalisé

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
+//Step 2
+//ajoute du texte avec des balises html
 document.getElementById("addText").innerHTML = "Hello World";
 
 	console.log('hello world');  
+
+
+//ajoute que du texte
+addText.textContent = "Hello World";
+//Step 3
+//element.removeChild(remove);
+
+//exercice2
+var removeId = document.getElementById('remove');
+removeId.remove();
+
+//exercice 3
+var changeColor = document.getElementById('changeColor');
+changeColor.style.backgoundColor = 'red';
+
+// Exercice 5
+
+var addChildren = document.getElementById('addChildren');
+var child;
+
+for (var i = 0; i < 4.; i++) {
+	addChildren.innerHTML += "<p>blabla</p>";
+}
